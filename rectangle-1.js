37d1fd4d0bb2b38efbc9d7665120d4ffbe9a5001
yezhijian <ttyyee@21cn.com>
2016-04-17 00:18:35 +0800

    my first commit rectangle-1

---
+++ rectangle-1.js
@@ -0,0 +1,12 @@
+/**
+ * Created by bastone on 16-4-15.
+ */
+
+var rect={
+    perimeter: function(x,y){ return 2*(x+y)},
+    area: function(x,y){return x*y}
+};
+
+module.exports=rect;
+
+
---
+++ solve-1.js
@@ -0,0 +1,8 @@
+/**
+ * Created by bastone on 16-4-15.
+ */
+
+var rect=require('./rectangle-1.js');
+
+console.log(rect.area(3,2.4));
+console.log(rect.perimeter(3,2.4));
